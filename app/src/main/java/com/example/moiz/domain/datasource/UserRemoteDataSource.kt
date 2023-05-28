a8b5325d60132e87df260c2ffefb55afaed7170b
JunYoung Lee <leejy8506@gmail.com>
2023-05-28 15:01:34 +0900

    [Feat] 데이터소스 제거

--- app/src/main/java/com/example/moiz/domain/datasource/UserRemoteDataSource.kt
+++
@@ -1,8 +0,0 @@
-package com.example.moiz.domain.datasource
-
-import com.example.moiz.data.network.NetworkResult
-import com.example.moiz.data.network.dto.UserResponseDto
-
-interface UserRemoteDataSource {
-    suspend fun getKakaoToken(token: String): UserResponseDto
-}
