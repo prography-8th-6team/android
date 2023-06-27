47f447be087be9da4b73a55eeb07f4ca4c8aced0
JunYoung Lee <leejy8506@gmail.com>
2023-06-27 23:17:30 +0900

    [Feat] 여행 디테일 화면 탭 레이아웃 + 뷰페이저로 구성

--- app/src/main/java/com/example/moiz/presentation/detail/TravelDetailFragment.kt
+++ app/src/main/java/com/example/moiz/presentation/detail/TravelDetailFragment.kt
@@ -6,26 +6,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.PopupWindow
+import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.asLiveData
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
 import com.example.moiz.R
 import com.example.moiz.data.UserDataStore
-import com.example.moiz.data.network.dto.BillingDto
 import com.example.moiz.databinding.ItemTravelMemberBinding
 import com.example.moiz.databinding.TravelDetailFragmentBinding
+import com.google.android.material.tabs.TabLayoutMediator
 import dagger.hilt.android.AndroidEntryPoint
-import java.util.Currency
+import timber.log.Timber
 
-@AndroidEntryPoint class TravelDetailFragment : Fragment() {
+@AndroidEntryPoint
+class TravelDetailFragment : Fragment() {
 
     private lateinit var binding: TravelDetailFragmentBinding
-    private lateinit var adapter: BillingAdapter
     private val viewModel: DetailViewModel by viewModels()
     private val args: TravelDetailFragmentArgs by navArgs()
+    private var token: String = ""
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -40,27 +42,76 @@ import java.util.Currency
         super.onViewCreated(view, savedInstanceState)
 
         UserDataStore.getUserToken(requireContext()).asLiveData().observe(viewLifecycleOwner) {
+            token = "Bearer $it"
             viewModel.getTravelDetail(
-                args.travelId, "Bearer $it")
+                args.travelId, "Bearer $it"
+            )
+        }
+        initViews()
+        initViewPager()
+    }
+
+    private fun initViewPager() {
+        var viewPagerAdapter = ViewPagerAdapter(requireActivity())
+        viewPagerAdapter.addFragment(BillingFragment())
+        viewPagerAdapter.addFragment(BillingFragment())
+        viewPagerAdapter.addFragment(BillingFragment())
+
+        binding.vpViewpagerMain.apply {
+            adapter = viewPagerAdapter
+
+            registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    // viewPager
+                    val view =
+                        (binding.vpViewpagerMain[0] as RecyclerView).layoutManager?.findViewByPosition(
+                            position
+                        )
+                    view?.post {
+                        val wMeasureSpec =
+                            View.MeasureSpec.makeMeasureSpec(view.width, View.MeasureSpec.EXACTLY)
+                        val hMeasureSpec =
+                            View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
+                        view.measure(wMeasureSpec, hMeasureSpec)
+                        if (binding.vpViewpagerMain.layoutParams.height != view.measuredHeight) {
+                            binding.vpViewpagerMain.layoutParams =
+                                (binding.vpViewpagerMain.layoutParams).also { lp ->
+                                    lp.height = view.measuredHeight
+                                }
+                        }
+                    }
+                }
+            })
         }
 
-        initViews()
+        TabLayoutMediator(binding.tlNavigationView, binding.vpViewpagerMain) { tab, position ->
+            when (position) {
+                0 -> tab.text = "가계부"
+                1 -> tab.text = "계산도우미"
+                2 -> tab.text = "일정"
+            }
+        }.attach()
     }
 
     private fun initViews() = with(binding) {
         ivAdditional.setOnClickListener {
             val inflater =
-                view.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+                view?.context?.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
             val popupView = inflater.inflate(R.layout.item_travel_detail_popup, null)
 
             val popupWindow = PopupWindow(
-                popupView, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+                popupView, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
+            )
             popupWindow.isOutsideTouchable = true
             popupWindow.isFocusable = true
             popupWindow.showAsDropDown(ivAdditional, -80, 20)
 
             popupView.findViewById<View>(R.id.tv_share).setOnClickListener {
-                // 여행 공유
+                viewModel.postGenerateInviteToken(args.travelId, token)
+                    .observe(viewLifecycleOwner) {
+                        Timber.d("share token: $it")
+                    }
             }
             popupView.findViewById<View>(R.id.tv_edit).setOnClickListener {
                 // 여행 수정
@@ -71,26 +122,16 @@ import java.util.Currency
         }
 
         nsvTravelDetail.run {
-            header = llDetailTab
-            temp = llToolbar
-        }
-
-        ivAddAccount.setOnClickListener {
-            findNavController().navigate(R.id.goto_add_billing)
+            nsvTravelDetail.run {
+                header = tlNavigationView
+                temp = llToolbar
+            }
         }
 
         viewModel.list.observe(viewLifecycleOwner) { data ->
             tvTitle.text = data.title
             tvData.text = data.start_date + "~" + data.end_date
             tvMemo.text = data.description
-            val currencySymbol = Currency.getInstance(data.currency).symbol
-            tvMyCost.text = currencySymbol + " " + data.my_total_billing.toString()
-            tvTotalCost.text = currencySymbol + " " + data.total_amount.toString()
-
-            adapter = BillingAdapter(::itemOnClick)
-            data.billings.let { adapter.submitList(data.billings) }
-            rvAccounts.layoutManager = LinearLayoutManager(context)
-            rvAccounts.adapter = adapter
 
             llTravelMember.apply {
                 data.members?.forEach {
@@ -102,9 +143,4 @@ import java.util.Currency
             }
         }
     }
-
-    private fun itemOnClick(data: BillingDto) {
-        // 가계부 상세 이동
-    }
-
 }
