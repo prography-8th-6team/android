f851cb85390cf76e1551686fcb24e5720cf5520b
JunYoung Lee <leejy8506@gmail.com>
2023-07-03 00:40:04 +0900

    [Feat] 가계부 상세 이미지 처리

--- app/src/main/java/com/example/moiz/presentation/billing/detail/BillingDetailFragment.kt
+++ app/src/main/java/com/example/moiz/presentation/billing/detail/BillingDetailFragment.kt
@@ -7,7 +7,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.asLiveData
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.example.moiz.R
 import com.example.moiz.data.UserDataStore
 import com.example.moiz.databinding.FragmentDetailBillingBinding
@@ -45,7 +47,7 @@ class BillingDetailFragment : Fragment() {
         }
 
         viewModel.data.observe(viewLifecycleOwner) {
-            ivBack.setOnClickListener { requireActivity().onBackPressed() }
+            ivBack.setOnClickListener { findNavController().popBackStack() }
             when (it.category) {
                 "food" -> ivCategory.setImageResource(R.drawable.ic_category_food)
                 "transportation" -> ivCategory.setImageResource(R.drawable.ic_category_transportation)
@@ -58,6 +60,26 @@ class BillingDetailFragment : Fragment() {
             val currencySymbol = Currency.getInstance(it.total_amount_currency).symbol
             tvBillingPrice.text = "$currencySymbol ${it.total_amount}"
             tvBillingDate.text = it.paid_date
+
+            when (it.images?.size) {
+                0 -> {
+                    ivEmptyImg.visibility = View.VISIBLE
+                }
+
+                1 -> {
+                    ivBillingImg.visibility = View.VISIBLE
+                    Glide.with(requireContext()).load(it.images[0])
+                        .into(ivBillingImg)
+                }
+
+                2 -> {
+                    llBillingImg.visibility = View.VISIBLE
+                    Glide.with(requireContext()).load(it.images[0])
+                        .into(ivBillingImg1)
+                    Glide.with(requireContext()).load(it.images[1])
+                        .into(ivBillingImg2)
+                }
+            }
 
             llBillingMembers.apply {
                 it.participants?.forEach { participant ->
