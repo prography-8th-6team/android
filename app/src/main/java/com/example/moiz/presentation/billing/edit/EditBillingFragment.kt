3e46248a18e740240559c001ac58acc585cc8a15
JunYoung Lee <leejy8506@gmail.com>
2023-07-08 10:32:12 +0900

    [Feat] 가계부 수정 화면

--- app/src/main/java/com/example/moiz/presentation/billing/edit/EditBillingFragment.kt
+++ app/src/main/java/com/example/moiz/presentation/billing/edit/EditBillingFragment.kt
@@ -20,6 +20,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.asLiveData
@@ -55,6 +56,10 @@ class EditBillingFragment : Fragment() {
     private lateinit var binding: FragmentEditBillingBinding
     private val viewModel: EditBillingViewModel by viewModels()
     private val args: EditBillingFragmentArgs by navArgs()
+    private var currencyList = ArrayList<Currency>()
+    private lateinit var adapter: AddBillingAdapter
+    var tempImgFile = arrayListOf<FileResult>()
+    var camUri: Uri? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -74,12 +79,41 @@ class EditBillingFragment : Fragment() {
         initViews()
     }
 
+    private fun itemOnClick(data: InputCostEntity) {
+        viewModel.updateCost(data)
+        binding.etPrice.setText(viewModel.totalAmount.toString())
+    }
+
+    private fun itemOnChange(data: InputCostEntity) {
+        viewModel.changeCost(data)
+        binding.etPrice.setText(viewModel.totalAmount.toString())
+    }
+
     private fun initViews() = with(binding) {
-        viewModel.data.observe(viewLifecycleOwner) { data ->
+        adapter = AddBillingAdapter(::itemOnClick, ::itemOnChange)
+        rvPaidForMembers.layoutManager = LinearLayoutManager(context)
+        rvPaidForMembers.adapter = adapter
+
+        viewModel.temp.observe(viewLifecycleOwner) {
+            it?.let { adapter.submitList(it) }
+        }
+
+        viewModel.isValidated.observe(viewLifecycleOwner) {
+            tvAddBilling.isEnabled = it
+            tvAddBilling.setBackgroundColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    if (it) R.color.color_F55C5C else R.color.color_F1F0F0
+                )
+            )
+        }
+
+        viewModel.paramList.observe(viewLifecycleOwner) { data ->
+            Timber.d("data: $data")
             etName.setText(data.title)
-            etPrice.setText(data.total_amount)
+            etPrice.setText(viewModel.totalAmount.toString())
             tvPickerDate.text = data.paid_date
-            setImgCnt(data.images?.size!!)
+            setImgCnt(viewModel.imageCnt)
             when (data.category) {
                 "food" -> ivCategory.setImageResource(R.drawable.ic_category_food)
                 "transportation" -> ivCategory.setImageResource(R.drawable.ic_category_transportation)
@@ -89,17 +123,319 @@ class EditBillingFragment : Fragment() {
                 else -> ivCategory.setImageResource(R.drawable.ic_category_other)
             }
         }
-    }
-
-    private fun setImgCnt(cnt: Int) {
-        if (cnt == 0) {
-            binding.ivImg.setImageResource(R.drawable.ic_unselect_img)
-            binding.tvImgCnt.visibility = View.GONE
-            return
+        etPrice.setOnEditorActionListener { _, _, _ ->
+            if (etPrice.text.isEmpty() || etPrice.text.toString().startsWith("0")) {
+                Toast.makeText(context, "금액을 입력해주세요.", Toast.LENGTH_SHORT).show()
+            } else {
+                val mInputMethodManager =
+                    requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                mInputMethodManager.hideSoftInputFromWindow(
+                    etPrice.windowToken,
+                    0
+                )
+                viewModel.totalAmount = etPrice.text.toString().toDouble()
+                viewModel.updateTotalAmount()
+                etPrice.clearFocus()
+            }
+            true
         }
 
-        binding.ivImg.setImageResource(R.drawable.ic_select_img)
-        binding.tvImgCnt.visibility = View.VISIBLE
-        binding.tvImgCnt.text = cnt.toString()
+        etName.setOnEditorActionListener { _, _, _ ->
+            val mInputMethodManager =
+                requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            mInputMethodManager.hideSoftInputFromWindow(
+                etName.windowToken,
+                0
+            )
+            viewModel.updateParam(0, etName.text.toString())
+            etName.clearFocus()
+            true
+        }
+
+        tvAuto.setOnClickListener {
+            adapter.billingType = false
+            etPrice.setText("")
+            etPrice.isEnabled = true
+            viewModel.clearCost()
+            tvAuto.setTextColor(resources.getColor(R.color.color_555555))
+            tvInput.setTextColor(resources.getColor(R.color.color_EBEAEA))
+        }
+
+        tvInput.setOnClickListener {
+            adapter.billingType = true
+            etPrice.setText("")
+            etPrice.isEnabled = false
+            viewModel.clearCost()
+            tvAuto.setTextColor(resources.getColor(R.color.color_EBEAEA))
+            tvInput.setTextColor(resources.getColor(R.color.color_555555))
+        }
+
+        ivTooltip.setOnClickListener {
+            val balloon = Balloon.Builder(requireContext())
+                .setWidthRatio(0.6f)
+                .setHeight(BalloonSizeSpec.WRAP)
+                .setText("직접 입력 시, 오차는 1%입니다.")
+                .setTextColorResource(R.color.color_555555)
+                .setBackgroundColorResource(R.color.white)
+                .setTextSize(14f)
+                .setArrowPositionRules(ArrowPositionRules.ALIGN_ANCHOR)
+                .setArrowSize(10)
+                .setArrowPosition(0.5f)
+                .setPadding(12)
+                .setCornerRadius(8f)
+                .setBalloonAnimation(BalloonAnimation.ELASTIC)
+                .build()
+            balloon.showAsDropDown(ivTooltip)
+            balloon.dismissWithDelay(1500L)
+        }
+
+        currencyList = arrayListOf(
+            Currency("$", "USD"),
+            Currency("€", "EUR"),
+            Currency("₩", "KRW"),
+            Currency("¥", "JPY"),
+            Currency("£", "GBP"),
+        )
+
+        binding.spnCurrency.adapter =
+            SpinnerAdapter(requireContext(), R.layout.spinner_currency_item_view, currencyList)
+        binding.spnCurrency.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                val currency = binding.spnCurrency.getItemAtPosition(p2) as Currency
+                viewModel.updateParam(2, currency.currencyText)
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
+        }
+
+        ivCategory.setOnClickListener {
+            val inflater =
+                view?.context?.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            val popupView = inflater.inflate(R.layout.item_billing_category, null)
+
+            val popupWindow =
+                PopupWindow(
+                    popupView,
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+                )
+            popupWindow.isOutsideTouchable = true
+            popupWindow.isFocusable = true
+            popupWindow.showAsDropDown(ivCategory, -132, 20)
+
+            popupView.findViewById<LinearLayout>(R.id.ll_shopping).setOnClickListener {
+                ivCategory.setImageResource(R.drawable.ic_category_shopping)
+                viewModel.updateParam(1, "shopping")
+                popupWindow.dismiss()
+            }
+
+            popupView.findViewById<LinearLayout>(R.id.ll_market).setOnClickListener {
+                ivCategory.setImageResource(R.drawable.ic_category_market)
+                viewModel.updateParam(1, "market")
+                popupWindow.dismiss()
+            }
+
+            popupView.findViewById<LinearLayout>(R.id.ll_food).setOnClickListener {
+                ivCategory.setImageResource(R.drawable.ic_category_food)
+                viewModel.updateParam(1, "food")
+                popupWindow.dismiss()
+            }
+
+            popupView.findViewById<LinearLayout>(R.id.ll_hotel).setOnClickListener {
+                ivCategory.setImageResource(R.drawable.ic_category_hotel)
+                viewModel.updateParam(1, "hotel")
+                popupWindow.dismiss()
+            }
+
+            popupView.findViewById<LinearLayout>(R.id.ll_transportation).setOnClickListener {
+                ivCategory.setImageResource(R.drawable.ic_category_transportation)
+                viewModel.updateParam(1, "transportation")
+                popupWindow.dismiss()
+            }
+
+            popupView.findViewById<LinearLayout>(R.id.ll_other).setOnClickListener {
+                ivCategory.setImageResource(R.drawable.ic_category_other)
+                viewModel.updateParam(1, "other")
+                popupWindow.dismiss()
+            }
+
+        }
+
+        ivImg.setOnClickListener {
+            val inflater =
+                view?.context?.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            val popupView = inflater.inflate(R.layout.item_billing_camera, null)
+
+            val popupWindow =
+                PopupWindow(
+                    popupView,
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+                )
+            popupWindow.isOutsideTouchable = true
+            popupWindow.isFocusable = true
+            popupWindow.showAsDropDown(ivImg, 0, 20)
+
+            popupView.findViewById<TextView>(R.id.tv_camera).setOnClickListener {
+                popupWindow.dismiss()
+                if (PermissionUtil.hasCameraPermission(requireContext())) {
+                    val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+                    val values = ContentValues(1)
+                    values.put(MediaStore.Images.Media.TITLE, "New Picture")
+                    camUri = requireContext().contentResolver.insert(
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                        values
+                    )
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, camUri)
+
+                    cameraLauncher.launch(intent)
+                } else {
+                    PermissionUtil.requestCameraPermission(requireActivity())
+                }
+            }
+
+            popupView.findViewById<TextView>(R.id.tv_gallery).setOnClickListener {
+                popupWindow.dismiss()
+                var intent = Intent(Intent.ACTION_PICK)
+                intent.data = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                intent.type = "image/*"
+                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+
+                launcher.launch(Intent.createChooser(intent, "파일을 선택해주세요."))
+            }
+        }
+
+        tvPickerDate.setOnClickListener {
+            var calendar = Calendar.getInstance()
+            var year = calendar.get(Calendar.YEAR)
+            var month = calendar.get(Calendar.MONTH)
+            var day = calendar.get(Calendar.DAY_OF_MONTH)
+            context?.let { it1 ->
+                DatePickerDialog(it1, { _, year, month, day ->
+                    run {
+                        val tempDate =
+                            year.toString() + "-" + (month + 1).toString() + "-" + day.toString()
+                        tvPickerDate.text = tempDate
+                        viewModel.updateParam(3, tempDate)
+                    }
+                }, year, month, day)
+            }?.show()
+        }
+
+        ivBack.setOnClickListener { findNavController().popBackStack() }
+
+        viewModel.members.observe(viewLifecycleOwner) {
+            it?.let {
+                val membersAdapter = ArrayAdapter(
+                    requireContext(),
+                    android.R.layout.simple_spinner_dropdown_item,
+                    it.map { it.name }
+                )
+
+                spMembers.adapter = membersAdapter
+                spMembers.onItemSelectedListener =
+                    object : AdapterView.OnItemSelectedListener {
+                        override fun onNothingSelected(parent: AdapterView<*>?) {
+
+                        }
+
+                        override fun onItemSelected(
+                            parent: AdapterView<*>?,
+                            view: View?,
+                            position: Int,
+                            id: Long
+                        ) {
+                            viewModel.updateParam(4, it[position].id!!)
+                        }
+                    }
+            }
+        }
+
+        tvAddBilling.setOnClickListener {
+            var tempAmount = 0.0
+            viewModel.paramList.value?.settlements?.forEach {
+                tempAmount += it?.amount!!
+            }
+
+            if (viewModel.totalAmount * 0.99 <= tempAmount && tempAmount <= viewModel.totalAmount * 1.01) {
+                UserDataStore.getUserToken(requireContext()).asLiveData()
+                    .observe(viewLifecycleOwner) {
+                        viewModel.putBilling(
+                            args.billingId,
+                            "Bearer $it",
+                            tempImgFile
+                        )
+                    }
+                // findNavController().popBackStack()
+            } else {
+                Toast.makeText(context, "비용과 체크된 금액의 합이 다릅니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
+
+    private var cameraLauncher: ActivityResultLauncher<Intent> = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        when (result.resultCode) {
+            Activity.RESULT_OK -> {
+                val imageUri = camUri
+                val fileInfo = getFileInfo(imageUri!!, requireContext())
+
+                if (tempImgFile.size < 2) {
+                    tempImgFile.add(fileInfo!!)
+                } else {
+                    Toast.makeText(context, "사진은 최대 2장까지 선택 가능합니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private var launcher: ActivityResultLauncher<Intent> = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        when (result.resultCode) {
+            Activity.RESULT_OK -> {
+                val data: Intent? = result.data
+
+                if (data?.clipData != null) {
+                    val count = data.clipData!!.itemCount
+                    for (i in 0 until count) {
+                        val imageUri = data.clipData!!.getItemAt(i).uri
+                        val fileInfo = getFileInfo(imageUri!!, requireContext())
+
+                        if (tempImgFile.size < 2) {
+                            tempImgFile.add(fileInfo!!)
+                            setImgCnt(tempImgFile.size)
+                        } else {
+                            Toast.makeText(context, "사진은 최대 2장까지 선택 가능합니다.", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                    }
+                } else if (data?.data != null) {
+                    val imageUri = data.data
+                    val fileInfo = getFileInfo(imageUri!!, requireContext())
+                    if (tempImgFile.size < 2) {
+                        tempImgFile.add(fileInfo!!)
+                        setImgCnt(tempImgFile.size)
+                    } else {
+                        Toast.makeText(context, "사진은 최대 2장까지 선택 가능합니다.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun setImgCnt(size: Int) {
+        if (tempImgFile.size == 0) {
+            binding.ivImg.setImageResource(R.drawable.ic_unselect_img)
+            binding.tvImgCnt.visibility = View.GONE
+        } else {
+            binding.ivImg.setImageResource(R.drawable.ic_select_img)
+            binding.tvImgCnt.visibility = View.VISIBLE
+            binding.tvImgCnt.text = tempImgFile.size.toString()
+        }
+    }
+
 }
--- app/src/main/java/com/example/moiz/presentation/billing/edit/EditBillingViewModel.kt
+++ app/src/main/java/com/example/moiz/presentation/billing/edit/EditBillingViewModel.kt
@@ -5,25 +5,192 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.moiz.data.network.dto.BillingDetailDto
+import com.example.moiz.data.network.dto.BillingMembersDto
+import com.example.moiz.data.network.dto.PostBillingDto
+import com.example.moiz.data.network.dto.SettlementsDto
+import com.example.moiz.domain.model.InputCostEntity
 import com.example.moiz.domain.usecase.GetBillingDetailUseCase
+import com.example.moiz.domain.usecase.GetBillingMembersUseCase
+import com.example.moiz.domain.usecase.PutBillingsUseCase
+import com.example.moiz.presentation.util.FileResult
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
-class EditBillingViewModel@Inject constructor(
-    private val getBillingDetailUseCase: GetBillingDetailUseCase
+class EditBillingViewModel @Inject constructor(
+    private val getBillingDetailUseCase: GetBillingDetailUseCase,
+    private val getBillingMembersUseCase: GetBillingMembersUseCase,
+    private val putBillingsUseCase: PutBillingsUseCase
 ) :
     ViewModel() {
 
-    private val _data = MutableLiveData<BillingDetailDto>()
-    val data: LiveData<BillingDetailDto> = _data
+    var totalAmount: Double = 0.0
+    var checkCnt: Int = 0
+    var imageCnt: Int = 0
+
+    private val _isValidated = MutableLiveData<Boolean>()
+    val isValidated: LiveData<Boolean> = _isValidated
+
+    private val _members = MutableLiveData<List<BillingMembersDto>>()
+    val members: LiveData<List<BillingMembersDto>> = _members
+
+    private val _temp = MutableLiveData<List<InputCostEntity>?>()
+    val temp: LiveData<List<InputCostEntity>?> = _temp
+
+    private val _paramList = MutableLiveData<PostBillingDto>()
+    val paramList: LiveData<PostBillingDto> = _paramList
 
     fun getBillingDetail(id: Int, token: String) {
         viewModelScope.launch {
             getBillingDetailUseCase.invoke(id, token).let {
-                _data.value = it
+                Timber.d("BillingDetailDto: $it")
+                totalAmount = it.total_amount?.toDouble()!!
+                imageCnt = it.images?.size!!
+                checkCnt = it.participants!!.size
+                _paramList.value = PostBillingDto(
+                    it.title,
+                    it.participants.find { participant ->
+                        participant.user?.nickname == it.paid_by
+                    }?.user?.id,
+                    it.paid_date,
+                    it.category,
+                    it.total_amount_currency,
+                    it.participants.map { participants ->
+                        SettlementsDto(
+                            participants.user?.id,
+                            participants.captured_amount?.toDouble()
+                        )
+                    }
+                )
             }
         }
+    }
+
+    fun putBilling(id: Int, token: String, imgList: List<FileResult>) {
+        viewModelScope.launch {
+            putBillingsUseCase.invoke(id, token, paramList.value!!, imgList)
+        }
+    }
+
+    fun getBillingMembers(id: Int, token: String) {
+        viewModelScope.launch {
+            getBillingMembersUseCase.invoke(id, token).let {
+                _members.value = it
+                _temp.value = it.map { member ->
+                    InputCostEntity(
+                        member.id!!,
+                        false,
+                        member.name!!,
+                        0.0
+                    )
+                }
+            }
+        }
+    }
+
+    private fun isValidate() = with(paramList.value!!) {
+        _isValidated.value = title != "" &&
+                category != "" &&
+                currency != "" &&
+                paid_date != "" &&
+                paid_by != 0 &&
+                settlements.isNotEmpty()
+    }
+
+    fun updateParam(type: Int, value: Any?) {
+        when (type) {
+            0 -> {
+                paramList.value?.title = value as String?
+                isValidate()
+            }
+
+            1 -> {
+                paramList.value?.category = value as String?
+                isValidate()
+            }
+
+            2 -> {
+                paramList.value?.currency = value as String?
+                isValidate()
+            }
+
+            3 -> {
+                paramList.value?.paid_date = value as String?
+                isValidate()
+            }
+
+            4 -> {
+                paramList.value?.paid_by = value as Int?
+                isValidate()
+            }
+
+            5 -> {
+                paramList.value?.settlements = _temp.value?.map {
+                    SettlementsDto(
+                        it.userId,
+                        it.cost
+                    )
+                }!!
+                isValidate()
+            }
+        }
+    }
+
+    fun updateTotalAmount() {
+        val temp = _temp.value?.toMutableList()
+        temp?.forEach {
+            if (it.isChecked) {
+                it.cost = totalAmount / checkCnt
+            } else {
+                it.cost = 0.0
+            }
+        }
+        _temp.value = temp
+
+        updateParam(5, null)
+    }
+
+    fun updateCost(data: InputCostEntity) {
+        if (data.isChecked) checkCnt++
+        else checkCnt--
+
+        val temp = _temp.value?.toMutableList()
+        temp?.forEach {
+            if (it.userId == data.userId) {
+                it.isChecked = data.isChecked
+            }
+        }
+        _temp.value = temp
+
+        updateTotalAmount()
+    }
+
+    fun changeCost(data: InputCostEntity) {
+        val temp = _temp.value?.toMutableList()
+        temp?.forEach {
+            if (it.userId == data.userId) {
+                totalAmount -= it.cost
+                totalAmount += data.cost
+                it.cost = data.cost
+            }
+        }
+        _temp.value = temp
+
+        updateParam(5, null)
+    }
+
+    fun clearCost() {
+        totalAmount = 0.0
+        checkCnt = 0
+        val temp = _temp.value?.toMutableList()
+        temp?.forEach {
+            it.isChecked = false
+            it.cost = 0.0
+        }
+        _temp.value = temp
+
+        updateParam(5, null)
     }
 }
