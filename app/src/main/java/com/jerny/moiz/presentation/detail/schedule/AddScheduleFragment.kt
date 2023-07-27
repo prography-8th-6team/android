5cd34b65f06a5d211ac46d6cc36ecbea9b02b0ab
JunYoung Lee <leejy8506@gmail.com>
2023-07-27 22:10:37 +0900

    [Feat] 일정 추가 화면 UI 작업

---
+++ app/src/main/java/com/jerny/moiz/presentation/detail/schedule/AddScheduleFragment.kt
@@ -0,0 +1,28 @@
+package com.jerny.moiz.presentation.detail.schedule
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.jerny.moiz.databinding.FragmentAddScheduleBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class AddScheduleFragment : Fragment() {
+
+    private lateinit var binding: FragmentAddScheduleBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        binding = FragmentAddScheduleBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
