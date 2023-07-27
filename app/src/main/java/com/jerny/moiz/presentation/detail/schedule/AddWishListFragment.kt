212e8ed52bebca861756539a6027df7858572005
JunYoung Lee <leejy8506@gmail.com>
2023-07-27 22:26:37 +0900

    [Feat] 장바구니 추가 화면 UI 작업

--- app/src/main/java/com/jerny/moiz/presentation/detail/schedule/AddWishListFragment.kt
+++ app/src/main/java/com/jerny/moiz/presentation/detail/schedule/AddWishListFragment.kt
@@ -17,19 +17,29 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.asLiveData
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.jerny.moiz.R
+import com.jerny.moiz.data.UserDataStore
 import com.jerny.moiz.databinding.FragmentAddWishListBinding
+import com.jerny.moiz.presentation.billing.BillingViewModel
+import com.jerny.moiz.presentation.billing.add.AddBillingFragmentArgs
 import com.jerny.moiz.presentation.util.FileResult
 import com.jerny.moiz.presentation.util.PermissionUtil
 import com.jerny.moiz.presentation.util.getFileInfo
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
 @AndroidEntryPoint
 class AddWishListFragment : Fragment() {
 
+    private val viewModel: AddScheduleViewModel by viewModels()
     private lateinit var binding: FragmentAddWishListBinding
+    private val args: AddWishListFragmentArgs by navArgs()
 
     var tempImgFile = arrayListOf<FileResult>()
     var camUri: Uri? = null
@@ -43,25 +53,43 @@ class AddWishListFragment : Fragment() {
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) = with(binding) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.imgBack.setOnClickListener {
+        viewModel.updateParam(2, "pending")
+
+        viewModel.isValidated.observe(viewLifecycleOwner) {
+            tvAddSchedule.isEnabled = it
+            tvAddSchedule.setBackgroundColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    if (it) R.color.color_F55C5C else R.color.color_F1F0F0
+                )
+            )
+        }
+
+        imgBack.setOnClickListener {
             findNavController().popBackStack()
         }
 
-        binding.root.setOnClickListener {
+        root.setOnClickListener {
             val mInputMethodManager =
                 requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             mInputMethodManager.hideSoftInputFromWindow(
-                binding.root.windowToken,
+                root.windowToken,
                 0
             )
-            binding.etName.clearFocus()
-            binding.etMemo.clearFocus()
+
+            if (etMemo.text.isNotEmpty())
+                viewModel.updateParam(1, etMemo.text.toString())
+            if (etName.text.isNotEmpty())
+                viewModel.updateParam(0, etName.text.toString())
+
+            etName.clearFocus()
+            etMemo.clearFocus()
         }
 
-        binding.ivCategory.setOnClickListener {
+        ivCategory.setOnClickListener {
             val inflater =
                 view?.context?.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
             val popupView = inflater.inflate(R.layout.item_billing_category, null)
@@ -77,49 +105,64 @@ class AddWishListFragment : Fragment() {
                 }
 
             val categoryClickListener: (Int) -> Unit = { resId ->
-                binding.ivCategory.setImageResource(resId)
+                ivCategory.setImageResource(resId)
                 popupWindow.dismiss()
             }
 
             popupView.findViewById<LinearLayout>(R.id.ll_shopping).setOnClickListener {
                 categoryClickListener(R.drawable.ic_category_shopping)
+                viewModel.updateParam(3, "shopping")
             }
 
             popupView.findViewById<LinearLayout>(R.id.ll_market).setOnClickListener {
                 categoryClickListener(R.drawable.ic_category_market)
+                viewModel.updateParam(3, "market")
             }
 
             popupView.findViewById<LinearLayout>(R.id.ll_food).setOnClickListener {
                 categoryClickListener(R.drawable.ic_category_food)
+                viewModel.updateParam(3, "food")
             }
 
             popupView.findViewById<LinearLayout>(R.id.ll_hotel).setOnClickListener {
                 categoryClickListener(R.drawable.ic_category_hotel)
+                viewModel.updateParam(3, "hotel")
             }
 
             popupView.findViewById<LinearLayout>(R.id.ll_transportation).setOnClickListener {
                 categoryClickListener(R.drawable.ic_category_transportation)
+                viewModel.updateParam(3, "transportation")
             }
 
             popupView.findViewById<LinearLayout>(R.id.ll_other).setOnClickListener {
                 categoryClickListener(R.drawable.ic_category_other)
+                viewModel.updateParam(3, "other")
             }
 
             popupWindow.showAsDropDown(binding.ivCategory, -132, 20)
         }
 
-        binding.etName.setOnEditorActionListener { _, _, _ ->
+        etName.setOnEditorActionListener { _, _, _ ->
             val mInputMethodManager =
                 requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             mInputMethodManager.hideSoftInputFromWindow(
-                binding.etName.windowToken,
+                etName.windowToken,
                 0
             )
-            binding.etName.clearFocus()
+
+            viewModel.updateParam(0, etName.text.toString())
+            etName.clearFocus()
             true
         }
 
-        binding.ivImg.setOnClickListener {
+        tvAddSchedule.setOnClickListener {
+            UserDataStore.getUserToken(requireContext()).asLiveData().observe(viewLifecycleOwner) {
+                viewModel.postSchedule("Bearer $it", args.travelId, tempImgFile)
+                findNavController().popBackStack()
+            }
+        }
+
+        ivImg.setOnClickListener {
             val inflater =
                 view?.context?.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
             val popupView = inflater.inflate(R.layout.item_billing_camera, null)
@@ -132,7 +175,7 @@ class AddWishListFragment : Fragment() {
                 )
             popupWindow.isOutsideTouchable = true
             popupWindow.isFocusable = true
-            popupWindow.showAsDropDown(binding.ivImg, 0, 20)
+            popupWindow.showAsDropDown(ivImg, 0, 20)
 
             popupView.findViewById<TextView>(R.id.tv_camera).setOnClickListener {
                 popupWindow.dismiss()
@@ -217,14 +260,14 @@ class AddWishListFragment : Fragment() {
         }
     }
 
-    private fun setImgCnt() {
+    private fun setImgCnt() = with(binding) {
         if (tempImgFile.size == 0) {
-            binding.ivImg.setImageResource(R.drawable.ic_unselect_img)
-            binding.tvImgCnt.visibility = View.GONE
+            ivImg.setImageResource(R.drawable.ic_unselect_img)
+            tvImgCnt.visibility = View.GONE
         } else {
-            binding.ivImg.setImageResource(R.drawable.ic_select_img)
-            binding.tvImgCnt.visibility = View.VISIBLE
-            binding.tvImgCnt.text = tempImgFile.size.toString()
+            ivImg.setImageResource(R.drawable.ic_select_img)
+            tvImgCnt.visibility = View.VISIBLE
+            tvImgCnt.text = tempImgFile.size.toString()
         }
     }
 }
