b4a732a3d96d98e1c475680167c766c620064457
Jiwon <senta2006@naver.com>
2023-08-06 22:45:26 +0900

    [Feat] 일정 순서, 메모, 시간, 카테고리 색상 표시

--- app/src/main/java/com/jerny/moiz/presentation/detail/schedule/ScheduleAdapter.kt
+++ app/src/main/java/com/jerny/moiz/presentation/detail/schedule/ScheduleAdapter.kt
@@ -1,20 +1,58 @@
 package com.jerny.moiz.presentation.detail.schedule
 
+import android.content.Context
+import android.content.res.ColorStateList
+import android.content.res.Resources
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.FrameLayout.LayoutParams
+import android.widget.LinearLayout
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.jerny.moiz.R
 import com.jerny.moiz.data.network.dto.ScheduleDto
 import com.jerny.moiz.databinding.ItemScheduleBinding
+import com.jerny.moiz.domain.model.Category
 
-class ScheduleAdapter() : ListAdapter<ScheduleDto, ScheduleAdapter.ViewHolder>(
+class ScheduleAdapter(private val context: Context) : ListAdapter<ScheduleDto, ScheduleAdapter.ViewHolder>(
     DiffCallback) {
+    val Int.dp: Int
+        get() = (this * Resources.getSystem().displayMetrics.density + 0.5f).toInt()
+
     inner class ViewHolder(private val binding: ItemScheduleBinding) : RecyclerView.ViewHolder(
         binding.root) {
         fun bind(schedule: ScheduleDto) {
             binding.tvName.text = schedule.title
-            binding.tvTime.text = schedule.start_at
+            binding.tvOrder.text = "${adapterPosition + 1}"
+
+            val totalParams =
+                LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
+            val scheduleParams =
+                LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
+            if (schedule.end_at.isNullOrEmpty()) {
+                binding.tvTime.text = "${schedule.start_at}"
+                totalParams.setMargins(37.dp, 0, 16.dp, 0)
+                scheduleParams.setMargins(38.dp, 0, 0, 0)
+
+            } else {
+                binding.tvTime.text = "${schedule.start_at}-${schedule.end_at}"
+                totalParams.setMargins(19.dp, 0, 16.dp, 0)
+                scheduleParams.setMargins(17.dp, 0, 0, 0)
+            }
+            binding.llTotal.layoutParams = totalParams
+            binding.llSchedule.layoutParams = scheduleParams
+
+            // 카테고리에 맞게 색상 설정
+            binding.tvOrder.backgroundTintList = when (schedule.category) {
+                Category.Shopping.value -> ColorStateList.valueOf(context.getColor(R.color.color_f9b7a4))
+                Category.Sights.value -> ColorStateList.valueOf(context.getColor(R.color.color_d8f4f1))
+                Category.Food.value -> ColorStateList.valueOf(context.getColor(R.color.color_f8f2c3))
+                Category.Hotel.value -> ColorStateList.valueOf(context.getColor(R.color.color_a4e8c0))
+                else -> ColorStateList.valueOf(context.getColor(R.color.color_abe8ff))
+            }
+
+            binding.tvDescription.text = schedule.description
         }
     }
 
