0edca9382f67495876fce47d48f951c0435b1fc5
JunYoung Lee <47595801+Lee-Jun-Young@users.noreply.github.com>
2023-08-13 00:13:43 +0900

    일정, 장바구니 추가 화면 (#49)
    * [Feat] 일정 추가 화면 UI 작업
    
    * [Feat] 장바구니 추가 화면 UI 작업
    
    * [Feat] 여행 상세 일정, 장바구니 추가 화면 연결
    
    * [Feat] 장바구니 추가 화면 ui 기능 작성
    
    * [Feat] 이미지 카운트 텍스트 추가
    
    * [Feat] 일정 프래그먼트 ui 기능 추가
    
    * [Feat] 일정 추가 화면 UI 작업
    
    * [Feat] 장바구니 추가 화면 UI 작업
    
    * [Feat] 여행 상세 일정, 장바구니 추가 화면 연결
    
    * [Feat] 장바구니, 일정 추가 api 코드 작성
    
    * [Feat] 장바구니 추가 화면 파라미터 넘기기 및 viewModel 연결
    
    * [Feat] 장바구니 추가 api 연결 확인
    
    * [Feat] 일정 추가 api 연결

--- app/src/main/java/com/jerny/moiz/data/network/dto/ScheduleDto.kt
+++ app/src/main/java/com/jerny/moiz/data/network/dto/ScheduleDto.kt
@@ -4,50 +4,68 @@ import com.google.gson.annotations.SerializedName
 
 data class ScheduleListDto(
     @SerializedName("id")
-    val id:Int?,
+    val id: Int?,
     @SerializedName("description")
-    val description:String?,
+    val description: String?,
     @SerializedName("members")
     val members: List<String>?,
     @SerializedName("title")
-    val title:String?,
+    val title: String?,
     @SerializedName("start_date")
-    val start_date:String?,
+    val start_date: String?,
     @SerializedName("end_date")
-    val end_date:String?,
+    val end_date: String?,
     @SerializedName("schedules")
-    val schedules:List<ScheduleDto>?
+    val schedules: List<ScheduleDto>?
 )
 
 data class ScheduleDto(
     @SerializedName("travel")
-    val travel:Int?,
+    val travel: Int?,
     @SerializedName("images")
-    val images:List<String?>,
+    val images: List<String?>,
     @SerializedName("id")
-    val id:Int?,
+    val id: Int?,
     @SerializedName("title")
-    val title:String?,
+    val title: String?,
     @SerializedName("description")
-    val description:String?,
+    val description: String?,
     @SerializedName("type")
-    val type:Int?,
+    val type: Int?,
     @SerializedName("category")
-    val category:String?,
+    val category: String?,
     @SerializedName("date")
-    val date:String?,
+    val date: String?,
     @SerializedName("start_at")
-    val start_at:String?,
+    val start_at: String?,
     @SerializedName("end_at")
-    val end_at:String?,
+    val end_at: String?,
     @SerializedName("created")
-    val created:String?,
+    val created: String?,
     @SerializedName("updated")
-    val updated:String?,
+    val updated: String?,
 )
+
 data class ResponseScheduleListDto(
     @SerializedName("message")
     val message: String?,
     @SerializedName("results")
     val results: ScheduleListDto?,
+)
+
+data class PostScheduleDto(
+    @SerializedName("type")
+    var type: String?,
+    @SerializedName("title")
+    var title: String?,
+    @SerializedName("description")
+    var description: String?,
+    @SerializedName("start_at")
+    var start_at: String?,
+    @SerializedName("end_at")
+    var end_at: String?,
+    @SerializedName("date")
+    var date: String?,
+    @SerializedName("category")
+    var category: String?
 )
