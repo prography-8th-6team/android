19833ea07ba4bba5a7eb184667c6b9e134de1370
JunYoung Lee <leejy8506@gmail.com>
2023-08-12 21:51:31 +0900

    [Feat] 장바구니, 일정 상세 기능

--- app/src/main/java/com/jerny/moiz/presentation/detail/schedule/detail/ScheduleDetailFragment.kt
+++ app/src/main/java/com/jerny/moiz/presentation/detail/schedule/detail/ScheduleDetailFragment.kt
@@ -1,23 +1,39 @@
 package com.jerny.moiz.presentation.detail.schedule.detail
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.content.res.ColorStateList
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.asLiveData
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.google.android.material.tabs.TabLayoutMediator
+import com.jerny.moiz.R
 import com.jerny.moiz.data.UserDataStore
+import com.jerny.moiz.data.network.dto.ScheduleDto
 import com.jerny.moiz.databinding.FragmentJourneyBinding
 import com.jerny.moiz.databinding.FragmentScheduleDetailBinding
 import com.jerny.moiz.databinding.TravelDetailFragmentBinding
+import com.jerny.moiz.presentation.billing.detail.ImageCustomDialog
 import com.jerny.moiz.presentation.detail.DetailViewModel
 import com.jerny.moiz.presentation.detail.TravelDetailFragmentArgs
 import com.jerny.moiz.presentation.detail.ViewPagerAdapter
+import com.jerny.moiz.presentation.util.CustomDialog
+import com.jerny.moiz.presentation.util.ScheduleDialog
+import com.jerny.moiz.presentation.util.gone
+import com.jerny.moiz.presentation.util.show
+import com.jerny.moiz.presentation.util.showOrGone
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
 import java.time.LocalDate
@@ -28,7 +44,6 @@ class ScheduleDetailFragment : Fragment() {
 
     private lateinit var binding: FragmentScheduleDetailBinding
     private val viewModel: ScheduleDetailViewModel by viewModels()
-    private val args: TravelDetailFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -40,16 +55,109 @@ class ScheduleDetailFragment : Fragment() {
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) = with(binding) {
         super.onViewCreated(view, savedInstanceState)
 
         UserDataStore.getUserToken(requireContext()).asLiveData().observe(viewLifecycleOwner) {
-            
+            viewModel.getScheduleDetail("Bearer $it", "49", 16)
         }
 
-        viewModel.scheduleData.observe(viewLifecycleOwner) {
-            Timber.d("Schedule Data: $it")
+        tvSchedule.setOnClickListener {
+            val dialog = ScheduleDialog() { date, start, end ->
+                setTop("confirmed")
+                viewModel.updateParam("confirmed", date, start, end)
+                scheduleGroup.show()
+                dpDate.text = date
+                dpStartDate.text = start
+                dpEndDate.text = end
+            }
+            dialog.isCancelable = false
+            dialog.show(requireActivity().supportFragmentManager, "schedule")
         }
+
+        tvWishList.setOnClickListener {
+            val dialog = CustomDialog("일정에서 장바구니로 이동시키면 날짜와 시간이 모두 초기화됩니다.", "취소", "장바구니로 이동") {
+                setTop("pending")
+                viewModel.updateParam("pending")
+                scheduleGroup.gone()
+            }
+            dialog.isCancelable = false
+            dialog.show(requireActivity().supportFragmentManager, "wishList")
+        }
+
+        ivBack.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
+        viewModel.scheduleData.observe(viewLifecycleOwner) { data ->
+            data?.let {
+                setTop(data.type!!)
+                tvBillingTitle.text = data.title
+                etMemo.text = data.description
+
+                scheduleGroup.showOrGone(data.type == "confirmed")
+                if (data.type == "confirmed") {
+                    dpDate.text = data.date
+                    dpStartDate.text = data.start_at
+                    dpEndDate.text = data.end_at
+                }
+
+                when (data.category) {
+                    "food" -> ivCategory.setImageResource(R.drawable.ic_category_food)
+                    "transportation" -> ivCategory.setImageResource(R.drawable.ic_category_transportation)
+                    "hotel" -> ivCategory.setImageResource(R.drawable.ic_category_hotel)
+                    "market" -> ivCategory.setImageResource(R.drawable.ic_category_market)
+                    "shopping" -> ivCategory.setImageResource(R.drawable.ic_category_shopping)
+                    else -> ivCategory.setImageResource(R.drawable.ic_category_other)
+                }
+
+                when (data.images.size) {
+                    0 -> {
+                        ivEmptyImg.visibility = View.VISIBLE
+                    }
+
+                    1 -> {
+                        ivBillingImg.visibility = View.VISIBLE
+                        Glide.with(requireContext()).load(data.images[0])
+                            .into(ivBillingImg)
+                        ivBillingImg.setOnClickListener { _ ->
+                            clickImage(data.images[0]!!)
+                        }
+                    }
+
+                    2 -> {
+                        llBillingImg.visibility = View.VISIBLE
+                        Glide.with(requireContext()).load(data.images[0])
+                            .into(ivBillingImg1)
+                        Glide.with(requireContext()).load(data.images[1])
+                            .into(ivBillingImg2)
+                        ivBillingImg1.setOnClickListener { _ ->
+                            clickImage(data.images[0]!!)
+                        }
+                        ivBillingImg2.setOnClickListener { _ ->
+                            clickImage(data.images[1]!!)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun clickImage(imgUrl: String) {
+        val dialog = ImageCustomDialog(imgUrl)
+        dialog.isCancelable = true
+        dialog.show(requireActivity().supportFragmentManager, "img_detail")
+    }
+
+    private fun setTop(type: String) {
+        val isConfirmed = (type == "confirmed")
+        val whiteColor = ContextCompat.getColor(requireContext(), R.color.white)
+        val greyColor = ContextCompat.getColor(requireContext(), R.color.color_EBEAEA)
+
+        binding.tvSchedule.backgroundTintList =
+            ColorStateList.valueOf(if (isConfirmed) whiteColor else greyColor)
+        binding.tvWishList.backgroundTintList =
+            ColorStateList.valueOf(if (isConfirmed) greyColor else whiteColor)
     }
 
 }
--- app/src/main/java/com/jerny/moiz/presentation/detail/schedule/detail/ScheduleDetailViewModel.kt
+++ app/src/main/java/com/jerny/moiz/presentation/detail/schedule/detail/ScheduleDetailViewModel.kt
@@ -4,22 +4,52 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.jerny.moiz.data.network.dto.PostScheduleDto
 import com.jerny.moiz.data.network.dto.ScheduleDto
 import com.jerny.moiz.domain.usecase.GetScheduleDetailUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@HiltViewModel
 class ScheduleDetailViewModel @Inject constructor(
     private val getScheduleDetailUseCase: GetScheduleDetailUseCase
 ) : ViewModel() {
     private val _scheduleData = MutableLiveData<ScheduleDto?>()
     val scheduleData: LiveData<ScheduleDto?> = _scheduleData
 
+    private var paramList: MutableLiveData<PostScheduleDto> = PostScheduleDto(
+        title = null,
+        description = null,
+        type = null,
+        category = null,
+        date = null,
+        start_at = null,
+        end_at = null
+    ).let { MutableLiveData(it) }
+
     fun getScheduleDetail(token: String, travelId: String, id: Int) {
         viewModelScope.launch {
             getScheduleDetailUseCase.invoke(token, travelId, id).let {
                 _scheduleData.value = it.results
+                paramList.value?.title = it.results?.title
+                paramList.value?.description = it.results?.description
+                paramList.value?.type = it.results?.type
+                paramList.value?.category = it.results?.category
+                paramList.value?.date = it.results?.date
+                paramList.value?.start_at = it.results?.start_at
+                paramList.value?.end_at = it.results?.end_at
             }
         }
     }
+
+    fun updateParam(type: String, date: String? = null, startAt: String? = null, endAt: String? = null) {
+        paramList.value?.type = type
+        paramList.value?.date = date
+        paramList.value?.start_at = startAt
+        paramList.value?.end_at = endAt
+
+
+    }
+
 }
