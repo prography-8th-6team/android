b61887e3bb0e8cea7532f1616d5acee4645cb5e3
JunYoung Lee <leejy8506@gmail.com>
2023-08-18 22:46:41 +0900

    [Feat] 위시리스트 -> 일정 상세 넘어갈 때 날짜, 시간 디폴트 값 설정, 테마 설정

--- app/src/main/java/com/jerny/moiz/presentation/util/ScheduleDialog.kt
+++ app/src/main/java/com/jerny/moiz/presentation/util/ScheduleDialog.kt
@@ -19,15 +19,20 @@ import com.jerny.moiz.databinding.DialogScheduleBinding
 import com.jerny.moiz.presentation.createTravelList.DatePickerDialog
 import java.text.SimpleDateFormat
 import java.util.Calendar
+import java.util.Date
 
 class ScheduleDialog(
     private val okClickListener: (String, String, String) -> Unit
 ) : DialogFragment() {
     lateinit var binding: DialogScheduleBinding
 
-    private var date = ""
-    private var startDate = ""
-    private var endDate = ""
+    private val currentDateTime = Calendar.getInstance().time
+    private val dateFormat = SimpleDateFormat("yyyy.MM.dd")
+    private val timeFormat = SimpleDateFormat("HH:mm")
+
+    private var date = dateFormat.format(currentDateTime)
+    private var startDate = timeFormat.format(currentDateTime)
+    private var endDate = timeFormat.format(currentDateTime)
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,6 +40,9 @@ class ScheduleDialog(
         savedInstanceState: Bundle?,
     ): View? {
         binding = DialogScheduleBinding.inflate(inflater, container, false)
+        binding.dpDate.text = date
+        binding.dpStartDate.text = startDate
+        binding.dpEndDate.text = endDate
 
         binding.dpDate.setOnClickListener {
             val datePickerFragment = DatePickerDialog()
